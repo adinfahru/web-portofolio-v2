cc7d18c2bdb395281aeff129abf312f56627036b
adinfahru <mohamad.fahrudin18@gmail.com>
2025-04-19 04:00:49 +0700

    add astro vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import node from "@astrojs/node";
-
+import vercel from "@astrojs/vercel"; // ganti node dengan vercel
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), react()],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "server", // boleh tetap pakai "server"
+  adapter: vercel(),
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
