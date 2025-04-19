20c284079db75f3071e36b3c4fd13c0fbc0cc5c1
adinfahru <mohamad.fahrudin18@gmail.com>
2025-04-19 10:36:50 +0700

    update static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import vercel from "@astrojs/vercel"; // ganti node dengan vercel
 import react from "@astrojs/react";
 
 export default defineConfig({
+  output: "static", // Ubah dari "server" ke "static"
   integrations: [tailwind(), icon(), react()],
-  output: "server", // boleh tetap pakai "server"
-  adapter: vercel(),
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
